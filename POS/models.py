f7066f01cb1c8f3e4dc065fce0975a57e497630c
Vacharavat <61070192@kmitl.ac.th>
2020-03-09 14:19:21 +0700

    update3

--- POS/models.py
+++ POS/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
-
 # Create your models here.
+
 class product_type(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return '%s (%s)'%(self.name, self.description)
 
 
 
 class product(models.Model):
     name = models.CharField(max_length=50) 
-    product_type = models.ForeignKey(product_type, on_delete=models.CASCADE)
+    Type = models.ForeignKey(product_type, on_delete=models.CASCADE, default='')
     description = models.CharField(max_length=200, null=True)
-    price = models.IntegerField()
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '%s (%s)'%(self.name, self.description)
